1955f143fa8aad3bd61d6ecc323324504bc22145
unknown <shubhamsinghal12@gmail.com>
2024-12-29 18:32:54 +0530

    Intermediate

---
+++ Python-Intermediate/calculator.py
@@ -0,0 +1,26 @@
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
+
+
+PI = 3.14
+X = 10
+CONSTANT = 200
+
+class A:
+    def __init__(self,x):
+        self.x = x
+
+
+if __name__ == "__main__":
+    print("Hi! Welcome to calculator")
+else:
+    print("Hello I am Calculator Module")
