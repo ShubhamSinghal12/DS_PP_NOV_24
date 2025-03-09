41fed3f63ec8029c4b6ad367c92c51dbbf95a062
unknown <shubhamsinghal12@gmail.com>
2025-03-09 12:10:33 +0530

    FaceRecog

--- OpenCVBasic/.ipynb_checkpoints/OpenCV-checkpoint.py
+++ OpenCVBasic/.ipynb_checkpoints/OpenCV-checkpoint.py
@@ -11,5 +11,5 @@ img = cv2.imread("./dog.png")
 cv2.imshow('Dog Image',img)
 # cv2.imshow('Gray Dog Image',gray)
 
-cv2.waitKey(5000) # Program will stop when any key is pressed
+cv2.waitKey(0) # Program will stop when any key is pressed
 cv2.destroyAllWindows()
--- OpenCVBasic/.ipynb_checkpoints/cv_video_read-checkpoint.py
+++ OpenCVBasic/.ipynb_checkpoints/cv_video_read-checkpoint.py
@@ -3,19 +3,51 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 while True:
-
-    ret,frame = cap.read()
-    gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-
+    
+    ret, frame = cap.read()
     if ret == False:
         continue
+    
+    cv2.imshow('Image',frame)
 
-    cv2.imshow("Video Capture",frame)
-    cv2.imshow("Gray Video",gray_frame)
-
-    key_pressed = cv2.waitKey(1)
-    if key_pressed == ord('q'):
+    key = cv2.waitKey(1)
+    if key == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import cv2
+
+# cap = cv2.VideoCapture(0)
+
+# while True:
+
+#     ret,frame = cap.read()
+#     gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+#     if ret == False:
+#         continue
+
+#     cv2.imshow("Video Capture",frame)
+#     cv2.imshow("Gray Video",gray_frame)
+
+#     key_pressed = cv2.waitKey(1)
+#     if key_pressed == ord('q'):
+#         break
+
+# cap.release()
+# cv2.destroyAllWindows()
